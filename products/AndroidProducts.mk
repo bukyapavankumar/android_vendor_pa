07f837157527813a4384f4e48181d17ddcb830dc
pavan kumar <bhukyapavan100@gmail.com>
2019-10-14 22:48:27 +0530

    update AndroidProducts.mk to include X00T

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -23,6 +23,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/oneplus6t/pa_oneplus6t.mk \
     $(LOCAL_DIR)/polaris/pa_polaris.mk \
     $(LOCAL_DIR)/sagit/pa_sagit.mk \
+    $(LOCAL_DIR)/X00T/pa_X00T.mk \
 
 # SONY OpenDevices
 # Loire 8956
@@ -50,4 +51,5 @@ COMMON_LUNCH_CHOICES += \
     pa_oneplus6t-userdebug \
     pa_polaris-userdebug \
     pa_sagit-userdebug \
-    pa_suzu-userdebug
+    pa_suzu-userdebug \
+    pa_X00T-userdebug
